ef57754d1ebecd6504b600a90036736a6715de3f
Pavel5284 <pavel5284@mail.ru>
2025-12-09 16:26:33 +0200

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { store } from "./app/store"
 import { HashRouter } from "react-router"
 
 createRoot(document.getElementById("root")!).render(
+  //BrowserRouter  changed to HashRouter for gh-pages
   <HashRouter>
     <Provider store={store}>
       <App />
